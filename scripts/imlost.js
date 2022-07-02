4f957128ec827af4e9b185da73e545be9edae194
kelvin <kelvinperrie@gmail.com>
2022-07-02 20:52:09 +1200

    just moving stuff around and fixing busted bits

--- scripts/imlost.js
+++ scripts/imlost.js
@@ -1,13 +1,66 @@
 
-// console.log("here's the data: ");
-// console.log(trailData);
+// our paths
+const vector = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        // CORS is a dick if you try to do this from a local file, so set it to what it will be when deployed
+        url: 'https://imlostinpukeiti.vercel.app/data/traildataRaw.gpx',
+        format: new ol.format.GPX(),
+    }),
+    style: function (feature) {
+        //var theType = feature.getGeometry().getType();
+        var myStyle = new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: '#000',
+                width: 3,
+            }),
+        })
+        return myStyle;
+    },
+});
 
-const parser = new DOMParser();
-const doc = parser.parseFromString(trailData, "application/xml");
-// print the name of the root element or error message
-const errorNode = doc.querySelector("parsererror");
-if (errorNode) {
-  console.log("error while parsing");
-} else {
-  console.log(doc.documentElement.nodeName);
-}
+// our nice picture
+const tile = new ol.layer.Tile({
+    source: new ol.source.OSM()
+});
+
+// set up the map, giving it the picture layer and the paths layer and centering on pukeiti
+map = new ol.Map({
+    target: 'map',
+    layers: [tile,vector],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([173.988,-39.193]),
+        zoom: 15
+    })
+});
+
+
+const displayFeatureInfo = function (pixel) {
+    const features = [];
+    map.forEachFeatureAtPixel(pixel, function (feature) {
+        features.push(feature);
+    });
+    if (features.length > 0) {
+        const info = [];
+        let i, ii;
+        for (i = 0, ii = features.length; i < ii; ++i) {
+        //info.push(features[i].get('desc'));
+        info.push(features[i].A.name);
+        }
+        document.getElementById('info').innerHTML = info.join(', ') || '(unknown)';
+        map.getTargetElement().style.cursor = 'pointer';
+        $("#info").show();
+    } else {
+        document.getElementById('info').innerHTML = '&nbsp;';
+        map.getTargetElement().style.cursor = '';
+        $("#info").hide();
+    }
+};
+
+map.on('pointermove', function (evt) {
+    if (evt.dragging) {
+        return;
+    }
+    $("#info").css({ top: (evt.originalEvent.offsetY + 20) + "px", left: evt.originalEvent.offsetX + "px" });
+    const pixel = map.getEventPixel(evt.originalEvent);
+    displayFeatureInfo(pixel);
+});
